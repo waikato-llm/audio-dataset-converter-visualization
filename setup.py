dabb79be06cc8e8d8b398b02bba68354c9800c9e
Peter Reutemann <fracpete@gmail.com>
2025-03-14 14:54:42 +1300

    prepared for release

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         "audio_dataset_converter>=0.0.1",
         "matplotlib",
     ],
-    version="0.0.1",
+    version="0.0.2",
     author='Peter Reutemann',
     author_email='fracpete@waikato.ac.nz',
     entry_points={
